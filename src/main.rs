1ded87059a9ca8a6b4a5e8e7b2c45cb1a872eb18
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-04-12 14:17:21 -0700

    :sparkles: connect and store

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use bevy::{prelude::*, window::WindowResolution};
+use bevy::{ecs::system::SystemState, prelude::*, window::WindowResolution};
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
 
-use midir::{Ignore, MidiInput, MidiInputPort};
+use midir::{Ignore, MidiInput, MidiInputConnection, MidiInputPort};
 
 // App state to store and manage notifications
 #[derive(Resource)]
@@ -11,8 +11,12 @@ pub struct MidiState {
     // Available ports
     available_ports: Vec<MidiInputPort>,
     // The ID of currently selected device's port
-    selected_port: Option<usize>,
+    selected_port: Option<MidiInputPort>,
 }
+
+// Event to trigger a notification
+#[derive(Default)]
+struct SelectDeviceEvent(usize);
 
 fn main() {
     App::new()
@@ -25,8 +29,10 @@ fn main() {
             ..default()
         }))
         .add_plugin(EguiPlugin)
+        .add_event::<SelectDeviceEvent>()
         .add_startup_system(setup_midi)
         .add_system(hello_world)
+        .add_system(discover_devices)
         .add_system(select_device)
         .add_system(select_device_ui)
         .run();
@@ -43,7 +49,7 @@ fn setup_midi(mut commands: Commands) {
     });
 }
 
-fn select_device(mut midi_state: ResMut<MidiState>) {
+fn discover_devices(mut midi_state: ResMut<MidiState>) {
     // Is there a device selected? Skip this system then.
     if midi_state.selected_port.is_some() {
         return;
@@ -53,16 +59,70 @@ fn select_device(mut midi_state: ResMut<MidiState>) {
     midi_state.available_ports = midi_state.input.ports();
 }
 
-fn select_device_ui(mut contexts: EguiContexts, mut midi_state: ResMut<MidiState>) {
+fn select_device(world: &mut World) {
+    let mut event_system_state = SystemState::<(EventReader<SelectDeviceEvent>)>::new(world);
+    let (mut device_events) = event_system_state.get(&world);
+
+    let mut connection_result = None;
+
+    if !device_events.is_empty() {
+        for device_event in device_events.iter() {
+            // Get the port from the event
+            let SelectDeviceEvent(device_id) = device_event;
+
+            let mut input =
+                MidiInput::new("midir reading input").expect("Couldn't initialize MidiInput");
+            input.ignore(Ignore::None);
+            let ports = input.ports();
+
+            match ports.get(*device_id).ok_or("invalid input port selected") {
+                Ok(device_port) => {
+                    println!("Connecting...");
+                    let _conn_in = input
+                        .connect(
+                            device_port,
+                            "midir-read-input",
+                            move |stamp, message, _| {
+                                println!("{}: {:?} (len = {})", stamp, message, message.len());
+
+                                // stamp = incrementing time
+                                // message = array of keyboard data. [keyEvent, keyId, strength]
+                                // key
+                            },
+                            (),
+                        )
+                        .expect("Couldn't connect to that port. Did the devices change recently?");
+                    connection_result = Some(_conn_in);
+                }
+                Err(error) => {
+                    println!("Error {}", error);
+                }
+            }
+            // let in_port_name = midi_state.input.port_name(device_id).expect("Couldn't connect to that port. Did the devices change recently?");
+        }
+
+        if let Some(connection) = connection_result {
+            world.insert_non_send_resource(connection);
+        }
+
+        // device_events.clear();
+    }
+}
+
+fn select_device_ui(
+    mut contexts: EguiContexts,
+    midi_state: Res<MidiState>,
+    mut device_event: EventWriter<SelectDeviceEvent>,
+) {
     let context = contexts.ctx_mut();
     egui::Window::new("Select a MIDI device").show(context, |ui| {
         let ports = midi_state.available_ports.iter().enumerate();
         for (index, port) in ports {
-            if ui
-                .button(midi_state.input.port_name(port).unwrap())
-                .clicked()
-            {
+            let device_name = midi_state.input.port_name(port).unwrap();
+            if ui.button(&device_name).clicked() {
                 // midi_state.selected_port = Some(index);
+                println!("Selecting device {}", &device_name);
+                device_event.send(SelectDeviceEvent(index));
             }
         }
     });
